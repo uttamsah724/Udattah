4b82cb8b07d4f2bd238729c6b49f8f7aefacf512
Uttam Sah <77891287+uttamsah724@users.noreply.github.com>
2021-08-30 15:04:36 +0530

    Update string.c

--- C/string.c
+++ C/string.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+// #include <string.h>
+// #include <math.h>
+// #include <stdlib.h>
+
+int main() 
+{
+	int i;
+    char s[100];
+    gets(s); 
+    // OR scanf("%[^\n]%*c",s);
+ 
+    // OR for(i=0;i<=24;i++)
+    //    scanf("%c", &s[i]);
+     
+  
+    
+    printf("Hello, World!\n");
+    puts(s);
+    // OR printf("%s",s);
+    
+    // OR for(i=0;i<=24;i++)
+    //    printf("%c",s[i]);
+    
+    return 0;
+}
 
