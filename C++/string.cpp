a25517916a1344b013f762182cf5087fd2b1815a
Uttam Sah <77891287+uttamsah724@users.noreply.github.com>
2021-08-31 21:26:41 +0530

    Update string.cpp

--- C++/string.cpp
+++ C++/string.cpp
@@ -1,1 +1,26 @@
+// In the first line print two space-separated integers, representing the length of  and  respectively.
+// In the second line print the string produced by concatenating  and  ().
+// In the third line print two strings separated by a space,  and .  and  are the same as  and , respectively, except that their first characters are swapped.
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s1,s2;
+	getline(cin,s1);
+    getline(cin,s2);                // cin>>s1 >> endl  >>s2;
+    
+    cout<<s1.length()<<" "<<s2.length()<<endl;
+    
+    cout<<s1+s2<<endl;
+    
+    char a=s1[0];
+    s1[0]=s2[0];
+    s2[0]=a;
+    cout<<s1<<" "<<s2<<endl;
+  
+    return 0;
+}
 
