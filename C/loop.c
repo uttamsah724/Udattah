c5dc64c07b43a458f33859a7ad9c6ab04f57a302
Uttam Sah <77891287+uttamsah724@users.noreply.github.com>
2021-08-31 19:56:14 +0530

    Update loop.c

--- C/loop.c
+++ C/loop.c
@@ -1,5 +1,6 @@
-// For each integer  in the interval  (given as input) :
+// SLOVE PROBLEM STEP BY STEP
 
+// For each integer  in the interval  (given as input) :
 // If , then print the English representation of it in lowercase. That is "one" for , "two" for , and so on.
 // Else if  and it is an even number, then print "even".
 // Else if  and it is an odd number, then print "odd".
