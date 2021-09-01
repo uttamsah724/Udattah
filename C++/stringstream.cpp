4e196f944d6308e236acc9e34f930f609ddf9906
Uttam Sah <77891287+uttamsah724@users.noreply.github.com>
2021-09-01 13:36:11 +0530

    Update stringstream.cpp

--- C++/stringstream.cpp
+++ C++/stringstream.cpp
@@ -1,1 +1,10 @@
-
+vector<int> parseInts(string str) {
+stringstream ss(str);   //??
+vector<int> result;
+char ch;
+int tmp;
+while(ss >> tmp) {      //??
+    result.push_back(tmp);
+    ss >> ch;           //??
+}
+return result;
